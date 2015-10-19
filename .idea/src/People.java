dd34f5173b82c4332b426f6e2f1d619cbcde4f55
benjamindrake <spcbdrake@yahoo.com>
2015-10-19 14:46:05 -0400

    final

--- .idea/src/People.java
+++ .idea/src/People.java
@@ -1,5 +1,35 @@
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
+
+import java.util.ArrayList;
+
 /**
  * Created by benjamindrake on 10/19/15.
  */
-public class People {
+public class People implements Comparable {
+    int id;
+    String first_name;
+    String last_name;
+    String email;
+    String country;
+    String ip_address;
+
+    public People(int id, String first_name, String last_name, String email, String country, String ip_address) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.country = country;
+        this.ip_address = ip_address;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        People c = (People) o;
+        return last_name.compareTo(c.last_name);
+
+    }
+    @Override
+    public String toString () {
+        return String.format("%s %s", first_name, last_name);
+    }
 }
---
+++ .idea/src/Peopleassignment.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
+
+/**
+ * Created by benjamindrake on 10/19/15.
+ */
+public class Peopleassignment {
+    public static void main(String[] args) {
+        HashMap<String, ArrayList<People>> people = new HashMap();
+
+        String postPeople = readFile("people.csv");
+
+        String[] lines = postPeople.split("\n");
+        int lineNum = 0;
+        for (String line : lines) {
+            if (lineNum !=0) {
+                String[] columns = line.split(",");
+                People people1 = new People(Integer.valueOf(columns[0]), columns[1], columns[2], columns[3], columns[4], columns[5]);
+                if (people.containsKey(columns[4])) {
+                    people.get(columns[4]).add(people1);
+                } else {
+                    ArrayList<People> peopleList = new ArrayList();
+                    peopleList.add(people1);
+                    people.put(columns[4], peopleList);
+                }
+            }
+            lineNum++;
+        }
+
+        for (Map.Entry<String, ArrayList<People>> list : people.entrySet()) {
+            Collections.sort(list.getValue());
+            System.out.println(String.format("%s %s", list.getKey(), list.getValue()));
+        }
+    }
+
+
+    static String readFile(String fileName) {
+        File f = new File(fileName);
+        try {
+            FileReader fr = new FileReader(f);
+            int fileSize = (int) f.length();
+            char[] fileContent = new char[fileSize];
+            fr.read(fileContent);
+            return new String(fileContent);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
